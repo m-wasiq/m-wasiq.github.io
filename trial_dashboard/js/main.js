7522e88193fbb0ca2e1e23aec3c7a0fc8656c743
m-wasiq <wasiq.mwa@gmail.com>
2021-08-03 19:22:13 +0500

    again

--- trial_dashboard/js/main.js
+++ trial_dashboard/js/main.js
@@ -18,3 +18,10 @@ function showSliderValue() {
   var bulletPosition = (rangeSlider.value - rangeSlider.min);
   rangeValue.style.top = (10*bulletPosition) + "px";
 }
+
+let vh = window.innerHeight * 0.01;
+document.documentElement.style.setProperty('--vh', `${vh}px`);
+window.addEventListener('resize', () => {
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+});
